d1351b7566b78e920dd39b5cdec2decb770216e5
Renan4485 <renanberto7@gmail.com>
2021-12-14 00:09:26 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello ";
+            var b = "world!";
+            Console.WriteLine(a + b);
+        }
+    }
+}
